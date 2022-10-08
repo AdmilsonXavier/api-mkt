bc0bc64d7ee68eddeb8538ccb69390b35b084e93
Admilson Xavier <admilson.xjr@gmail.com>
2022-10-08 01:05:58 -0300

    Ajuste json body

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 app.use('/', require('./route/productsRoute'));
+app.use('/', require('./route/brandsRoute'));
+app.use('/', require('./route/categoriesRoute'));
+app.use('/', require('./route/variationsRoute'));
 
 app.listen(3000);
 console.log("Servidor rodando na porta 3000...")
