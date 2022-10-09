79cbafd62a8d95f5f557a513915b74a6ac22bf91
Admilson Xavier <admilson.xjr@gmail.com>
2022-10-09 00:05:50 -0300

    Collection API

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
 const app = express();
 
 app.use(express.json());
@@ -6,6 +8,8 @@ app.use('/', require('./route/productsRoute'));
 app.use('/', require('./route/brandsRoute'));
 app.use('/', require('./route/categoriesRoute'));
 app.use('/', require('./route/variationsRoute'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 app.listen(3000);
 console.log("Servidor rodando na porta 3000...")
